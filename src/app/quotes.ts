cb0d7a30e539568d4a4adf52fc18e72ff32818d3
MugeraH <bwosihughes24@gmail.com>
2021-03-29 01:56:06 +0300

    update quote-form html to accept user input and get form to work

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -2,7 +2,7 @@ export class Quotes {
   constructor(
     public id: number,
     public quoteTitle: string,
-    public quote: string,
+    public quoteText: string,
     public authorName: string,
     public userName: string,
     public datePosted: Date,
