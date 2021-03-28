5d37ccc066afb64fb685bffaf8313a49150be208
MugeraH <bwosihughes24@gmail.com>
2021-03-28 17:05:29 +0300

    add quotes class to shoe data model

---
+++ src/app/quotes.ts
@@ -0,0 +1,15 @@
+export class Quotes {
+  public showDetails: boolean;
+  constructor(
+    public id: number,
+    public quoteTitle: string,
+    public quote: string,
+    public authorName: string,
+    public userName: string,
+    public datePosted: Date,
+    public likes: number,
+    public dislikes: string
+  ) {
+    this.showDetails = false;
+  }
+}
