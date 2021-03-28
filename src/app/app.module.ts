0804c8d27377a38bb2c3e027e710526463cdf4b4
MugeraH <bwosihughes24@gmail.com>
2021-03-29 01:40:42 +0300

    add formsmodule to app.component to allow use of ngForms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,13 +17,10 @@ import { QuoteItemDetailsComponent } from './quote-item-details/quote-item-detai
     QuotesListComponent,
     QuotesFormComponent,
     QuoteItemComponent,
-    QuoteItemDetailsComponent
+    QuoteItemDetailsComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
