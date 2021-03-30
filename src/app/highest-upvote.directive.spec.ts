540c307b7a8a4d20321b3f9493edda31b3f7214f
MugeraH <bwosihughes24@gmail.com>
2021-03-30 12:04:53 +0300

    Auto-generated commit

--- src/app/highest-upvote.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HighestUpvoteDirective } from './highest-upvote.directive';
-
-describe('HighestUpvoteDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HighestUpvoteDirective();
-    expect(directive).toBeTruthy();
-  });
-});
